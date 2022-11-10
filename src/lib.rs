a8ae14384407152f008a4e136cdd71e4273d03d7
Sojan James <sojan.james@gmail.com>
2022-11-10 19:20:45 +0530

    Added constructor for DltContext

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 include!("bindings.rs");
 
+impl DltContext {
+    /// Create an unitialized DltContext
+    pub const fn new_uninitialized() -> std::mem::MaybeUninit<Self> {
+        ::std::mem::MaybeUninit::uninit()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     //use super::*;
-
 
 }
--- wrapper.h
+++ wrapper.h
@@ -1,1 +1,2 @@
+#include<dlt/dlt_user.h>
 #include<dlt/dlt_client.h>
